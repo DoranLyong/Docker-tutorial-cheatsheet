7827a8256da6ebe066ec94ea951b8a9e4c7307d7
DoranLyong <cheeryun@gmail.com>
2021-06-30 00:56:02 +0900

    mybom

---
+++ etc/make_mybom_emergency.bash
@@ -0,0 +1,26 @@
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+
+docker run -it \
+    --name mybom_ros \
+    --gpus all \
+    --device=/dev/video0:/dev/video0 \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    -v $HOME:/workspace \
+    doranlyong/mybom_emergency:ros-melodic-v0.1\
+    bash
